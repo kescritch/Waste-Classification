5971536c1847f2b44de47da7bc21664ebc84032a
kescritch <kescritch@gmail.com>
2026-06-04 11:38:45 -0500

    debugging ecaluate_model

--- run.py
+++ run.py
@@ -3,11 +3,10 @@ import app.camera.camera_runner as camera
 import app.utils.calibrate_background as calibrate
 import app.utils.image_processor as image_processor
 import os
-from app.config import ROOT_DIR, MODELS_DIR
-image_processor.get_and_print_distribution(ROOT_DIR)
-# model_ver = "v2" # Choose 'v1' for custom CNN or 'v2' for transfer learning with MobileNetV2
 
-# model.main(model_ver) #builds the model
+model_ver = "v2" # Choose 'v1' for custom CNN or 'v2' for transfer learning with MobileNetV2
+
+model.main(model_ver) #builds the model
 
 # calibrate.run_calibration()
 # built_model = model.build_model(model_ver)
