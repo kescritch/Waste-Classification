4f48f2b74e41012722acff654cd824718941c17a
kescritch <kescritch@gmail.com>
2026-06-09 12:16:53 -0500

    Updated v1 custom model to match tensorflow v1

--- app/models/cnn_v1.py
+++ app/models/cnn_v1.py
@@ -1,27 +1,47 @@
 from torch import nn
 import torch.nn.functional as F
 
-class CNN_V1(nn.Module): 
-    def __init__(self, num_classes:int):
+class CNN_V1(nn.Module):
+    def __init__(self, num_classes: int = 5):
         super().__init__()
-        
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
-        
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
-        
-        self.fc1 = nn.Linear(56 * 56 * 64, num_classes)
-        
-    def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = self.pool(x)
-        x = F.relu(self.conv2(x))
-        x = self.pool(x)
-        
-        x = x.reshape(x.shape[0], -1) #flatten the output for the fully connected layer
-       
-        x = self.fc1(x)
 
+        # Conv Block 1
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+
+        # Conv Block 2
+        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+
+        # Conv Block 3
+        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+
+        # Conv Block 4
+        self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, stride=1, padding=1)
+        self.bn4 = nn.BatchNorm2d(256)
+
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # Global Average Pooling collapses each feature map to a single value
+        self.global_avg_pool = nn.AdaptiveAvgPool2d((1, 1))
+
+        # Dense layers
+        self.fc1 = nn.Linear(256, 256)
+        self.fc2 = nn.Linear(256, num_classes)
+
+        self.dropout = nn.Dropout(p=0.5)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.bn1(self.conv1(x))))
+        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = self.pool(F.relu(self.bn3(self.conv3(x))))
+        x = self.pool(F.relu(self.bn4(self.conv4(x))))
+
+        x = self.global_avg_pool(x)  # (batch, 256, 1, 1)
+        x = x.reshape(x.shape[0], -1)  # (batch, 256)
+
+        x = F.relu(self.fc1(x))
+        x = self.dropout(x)
+        x = self.fc2(x)
         return x
-        
