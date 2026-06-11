aa7e9e1f0c64face3fe1663cff21e1cc99620e36
kescritch <kescritch@gmail.com>
2026-06-11 12:18:48 -0500

    edited run.py

--- run.py
+++ run.py
@@ -1,19 +1,16 @@
 import app.main as model
 from app.utils import image_processor
 
-def main():
-    """
-    To run a saved model uncomment model.run_model and comment out model.build_model with the appropriate model version.
-    To save a new model uncomment model.build_model and comment out model.run_model.
-    """
+"""
+To run a saved model uncomment model.run_model and comment out model.build_model with the appropriate model version.
+To save a new model uncomment model.build_model and comment out model.run_model.
+"""
 
-    model_ver = "v2"
+model_ver = "v2"
 
-    # image_processor.get_and_print_distribution("app/data/training_data/archive")
+# image_processor.get_and_print_distribution("app/data/training_data/archive")
 
-    model.build_model(model_ver)
+model.build_model(model_ver)
 
-    # model.run_model(model_ver)
+# model.run_model(model_ver)
 
-if __name__ == "__main__":
-    main()
